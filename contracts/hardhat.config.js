f232f161b9dfb5fc5d1f9c84792fa793fbd313db
kalitva <33896868+pavave@users.noreply.github.com>
2025-08-05 07:50:49 +0300

    Update hardhat.config.js

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -7,7 +7,22 @@ module.exports = {
     holesky: {
       url: process.env.HOLESKY_RPC_URL,
       accounts: [process.env.PRIVATE_KEY],
-      chainId: 17000
-    }
-  }
+      chainId: 17000,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      holesky: process.env.ETHERSCAN_API_KEY, // Etherscan API для Holesky
+    },
+    customChains: [
+      {
+        network: "holesky",
+        chainId: 17000,
+        urls: {
+          apiURL: "https://api-holesky.etherscan.io/api",
+          browserURL: "https://holesky.etherscan.io",
+        },
+      },
+    ],
+  },
 };
