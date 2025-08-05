bb2c91f4b355270007455abdcd670ef91e454c4b
kalitva <33896868+pavave@users.noreply.github.com>
2025-08-05 07:43:17 +0300

    Update PublicationRegistry.sol

--- contracts/PublicationRegistry.sol
+++ contracts/PublicationRegistry.sol
@@ -3,28 +3,30 @@ pragma solidity ^0.8.20;
 
 contract PublicationRegistry {
     struct Publication {
-        string cid;         // IPFS CID (ссылка на публикацию)
-        address author;     // Автор публикации
-        uint256 timestamp;  // Когда добавлена
+        string cid;
+        string doi;
+        address author;
+        uint256 timestamp;
     }
 
     Publication[] public publications;
 
-    event PublicationAdded(uint256 indexed id, string cid, address indexed author, uint256 timestamp);
+    event PublicationAdded(uint256 indexed id, string cid, string doi, address indexed author, uint256 timestamp);
 
-    function addPublication(string calldata cid) external {
+    function registerPublication(string calldata cid, string calldata doi) external {
         publications.push(Publication({
             cid: cid,
+            doi: doi,
             author: msg.sender,
             timestamp: block.timestamp
         }));
 
-        emit PublicationAdded(publications.length - 1, cid, msg.sender, block.timestamp);
+        emit PublicationAdded(publications.length - 1, cid, doi, msg.sender, block.timestamp);
     }
 
-    function getPublication(uint256 id) external view returns (string memory, address, uint256) {
+    function getPublication(uint256 id) external view returns (string memory, string memory, address, uint256) {
         Publication memory pub = publications[id];
-        return (pub.cid, pub.author, pub.timestamp);
+        return (pub.cid, pub.doi, pub.author, pub.timestamp);
     }
 
     function totalPublications() external view returns (uint256) {
